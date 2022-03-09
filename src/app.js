6f3591ed777eeb041aa932ea26f0f9f21d7a8a22
Jenna Ransom <jenna.ransom@gmail.com>
2022-03-08 17:06:03 -0800

    adds movement and makes  a list of word objects

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
+let words
+
 function setup()  {
   createCanvas(windowWidth, windowHeight)
+  words = []
 }
 
 function draw() {
   background('#16ab59') //overwrite bckgd w/each draw to have single circle
+  words.forEach(w => {
+    w.draw()
+  }) 
+}
 
+function mouseClicked() {
   let w = new Word(mouseX, mouseY, 50)
-  w.draw()
+  words.push(w)
 }
--- src/word.js
+++ src/word.js
@@ -1,10 +1,16 @@
 class Word {
   constructor(x, y, size) {
     this.position = createVector(x, y)
+    this.speed = createVector(0, -5)
     this.size = size
   }
 
+  move() {
+    this.position = this.position.add(this.speed)
+  }
+
   draw() {
+    this.move()
     circle(this.position.x, this.position.y, this.size)
   }
 }
