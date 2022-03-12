ea8d10b0df45e8174bcbd929b385e6d73f87a96b
Jenna Ransom <jenna.ransom@gmail.com>
2022-03-11 16:12:43 -0800

    adds gravity and acceleration

--- src/word.js
+++ src/word.js
@@ -1,16 +1,19 @@
 class Word {
   constructor(x, y, size) {
     let randomAngle = random(-0.25 * PI, 0.25 * PI)
+    let mass = 0.25 + random(0, 0.5)
     this.position = createVector(x, y)
-    this.speed = createVector(0, -5)
+    this.speed = createVector(0, -10)
     this.speed = this.speed.rotate(randomAngle)
+    this.acceleration = createVector(0, mass)
     this.size = size
   }
 
   move() {
+    this.speed = this.speed.add(this.acceleration)
     this.position = this.position.add(this.speed)
     this.size = constrain(this.size -1, 0, 50)
-    this.speed = this.speed.rotate(PI * 0.01)
+    // this.speed = this.speed.rotate(PI * 0.01)
   }
 
   draw() {
